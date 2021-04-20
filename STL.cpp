c5ba612e52bcb9b5ff7cc706de8b02442d9ec631
diovch <ovchinnikov.dm16@physics.msu.ru>
2021-04-20 15:17:23 +0300

    WriteSTLBinary

--- STL.cpp
+++ STL.cpp
@@ -2,39 +2,53 @@
 #include "R3Graph.h"
 #include <fstream>
 //TODO Tune WriteBinarySTL()
-void WriteBinarySTL(const Triangulation& triangulation, std::string& filename)
+void WriteSTLBinary(const Triangulation& triangulation, std::string& filename)
 {
 	std::ofstream out;
 	filename = "C:\\Users\\owchi\\source\\repos\\TEST\\bin\\" + filename;
-	out.open(filename);
+	out.open(filename, std::ofstream::binary);
 	if (out.is_open())
 	{
-		out << "header" << std::endl;
-		out << triangulation.triangles.size() << std::endl;
-		out << "foreach triangle" << std::endl;
+
+		char header[80];
+		std::memset(header, 0, 80);
+		out.write(header, 80);
+
+		int numTriang = int(triangulation.triangles.size());
+		out.write((const char*)(&numTriang), 4);
+
+
 		for (int i = 0; i < triangulation.triangles.size(); ++i)
 		{
 			Triangulation::Triangle t = triangulation.triangles.at(i);
+			
+			R3Graph::R3Point p[4];
+			p[0] = triangulation.vertices.at(t.indices[0]).point;
+			p[1] = triangulation.vertices.at(t.indices[1]).point;
+			p[2] = triangulation.vertices.at(t.indices[2]).point;
+			p[3] = t.Normal;
 
-			R3Graph::R3Point p0 = triangulation.vertices.at(t.indices[0]).point;
-			R3Graph::R3Point p1 = triangulation.vertices.at(t.indices[1]).point;
-			R3Graph::R3Point p2 = triangulation.vertices.at(t.indices[2]).point;
-
-			out << t.Normal.x << " " << t.Normal.y << " " << t.Normal.z << std::endl;
-
-			out << p0.x << " " << p0.y << " " << p0.z << std::endl;
-			out << p1.x << " " << p1.y << " " << p1.z << std::endl;
-			out << p2.x << " " << p2.y << " " << p2.z << std::endl;
-
-			out << 0 << std::endl;
+			for (int i = 0; i < 4; ++i)
+			{
+				float vec[3];
+				vec[0] = (float) p[i].x;
+				vec[1] = (float) p[i].y;
+				vec[2] = (float) p[i].z;
+				out.write((const char*) vec, 12);
+			}
+		
+			unsigned short dummy = 48;
+			out.write((const char*)&dummy, 2);
 		}
-		out << "end";
 	}
 	out.close();
 }
 
-void WriteStlFile(const Triangulation& triangulation, std::ofstream& out)
+void WriteStlASCII(const Triangulation& triangulation, std::string& filename)
 {
+	std::ofstream out;
+	filename = "C:\\Users\\owchi\\source\\repos\\TEST\\bin\\" + filename;
+	out.open(filename);
 	out << "solid iso" << std::endl;
 
 	for (int i = 0; i < triangulation.triangles.size(); ++i) {
--- STL.h
+++ STL.h
@@ -2,9 +2,6 @@
 #include "Triangulation.h"
 #include <string>
 
-void WriteBinarySTL(const Triangulation&, std::string&);
+void WriteSTLBinary(const Triangulation&, std::string&);
 
-
-void WriteStlFile(const Triangulation& triangulation, std::ofstream& out);
-
-void WriteStlFile(const Triangulation& triangulation, std::ofstream& out, std::vector<std::set<Triangulation::Triangle>>& s);
+void WriteStlASCII(const Triangulation& triangulation, std::string& filename);
